577dfb7025725ea44183d9380ff20f4307130578
PramudithaN <pramudithanadun@gmail.com>
2024-07-10 22:36:52 +0530

    Mobile responsive navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,37 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
 	return (
 		<nav className="navbar">
 			<div className="navbar-logo">
 				<img src={"https://7rouaftcqgovbqs4.public.blob.vercel-storage.com/Logo-port-CumrPK6E6bR5sga87Ct2RRKiF9KpP4.png"} alt="Logo" />
 			</div>
-			{/* <div className="navbar-center-text">
-				PRAMA
-			</div> */}
-			<ul className="navbar-links">
+			<div className="menu-icon" onClick={toggleMenu}>
+				☰
+			</div>
+			<ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
 				<li>
-					<NavLink
-						to="/"
-						className={({ isActive }) => (isActive ? "active" : "")}
-					>
+					<NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
 						HOME
 					</NavLink>
 				</li>
 				<li>
-					<NavLink
-						to="/contact"
-						className={({ isActive }) => (isActive ? "active" : "")}
-					>
+					<NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>
 						CONTACT
 					</NavLink>
 				</li>
-        <li>
-					<NavLink
-						to="/sketch"
-						className={({ isActive }) => (isActive ? "active" : "")}
-					>
+				<li>
+					<NavLink to="/sketch" className={({ isActive }) => (isActive ? "active" : "")}>
 						SKETCH
 					</NavLink>
 				</li>
